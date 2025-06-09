d55f39ccee976bae7b58f65e60797250e2a77c31
hbbliyong <hbbliyong@163.com>
2025-06-09 11:30:32 +0800

    first commit

---
+++ src/hello.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lrx on 25-6-4.
+//
+#include "hello.h"
+#include <iostream>
+
+Hello::Hello() {
+}
+
+Hello::~Hello() {
+}
+
+void Hello::sayHello() {
+    std::cout << "Hello World!" << std::endl;
+}
---
+++ src/hello.h
@@ -0,0 +1,13 @@
+//
+// Created by lrx on 25-6-4.
+//
+
+#ifndef HELLO_H
+#define HELLO_H
+class Hello {
+    public:
+    Hello();
+    ~Hello();
+    void sayHello();
+};
+#endif //HELLO_H
